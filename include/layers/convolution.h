93300fe33577eb41f8b17a60b219c9d0d5b83f4a
thainq <thainguyen12.work@gmail.com>
2024-12-11 23:14:55 +0700

    add convolution

--- include/layers/convolution.h
+++ include/layers/convolution.h
@@ -14,16 +14,6 @@ std::tuple<int, int, int, int> calc_out_dims(
     const std::tuple<int, int>& padding
 );
 
-at::Tensor multiple_convs_kan_conv2d(
-    const at::Tensor& matrix,
-    const std::vector<std::shared_ptr<torch::nn::Conv2d>>& kernels,
-    int64_t kernel_side,
-    int64_t out_channels,
-    const std::pair<int64_t, int64_t>& stride = {1, 1},
-    const std::pair<int64_t, int64_t>& dilation = {1, 1},
-    const std::pair<int64_t, int64_t>& padding = {0, 0},
-    const torch::Device& device = torch::kCUDA);
-
 torch::Tensor add_padding(const torch::Tensor& matrix, const std::pair<int, int>& padding);
 
 #endif // CONVOLUTION_H
