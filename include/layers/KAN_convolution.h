cdf1fc358386ca9d63b8f133bd3ffe1436ebba2d
Xbacc <bachotbau@gmail.com>
2024-12-12 11:37:07 +0700

    add KAN Conv

---
+++ include/layers/KAN_convolution.h
@@ -0,0 +1,84 @@
+#ifndef KAN_CONV_H
+#define KAN_CONV_H
+
+#include <torch/torch.h>
+#include "../../include/layers/KAN_linear.h"
+#include "../../include/layers/convolution.h"
+
+class KAN_Convolution_Layer : public torch::nn::Module {
+    public:
+        KAN_Convolution_Layer(int in_channels = 1,
+                              int out_channels = 1,
+                              std::pair<int, int> kernel_size = {2,2},
+                              std::pair<int, int> stride = {1,1},
+                              std::pair<int, int> padding = {0,0},
+                              std::pair<int, int> dilation = {1,1},
+                              int grid_size = 5,
+                              int spline_order = 3,
+                              double scale_noise = 0.1,
+                              double scale_base = 1.0,
+                              double scale_spline = 1.0,
+                              torch::nn::SiLU base_activation = torch::nn::SiLU(),
+                              double grid_eps = 0.02,
+                              std::pair<double, double> grid_range = {-1, 1},
+                              std::string device = "cpu");
+        torch::Tensor forward(const torch::Tensor& x);
+
+    private:
+        int in_channels;
+        int out_channels;
+        std::pair<int, int> kernel_size;
+        std::pair<int, int> stride;
+        std::pair<int, int> padding;
+        std::pair<int, int> dilation;
+        int grid_size;
+        int spline_order;
+        double scale_noise;
+        double scale_base;
+        double scale_spline;
+        torch::nn::SiLU base_activation;
+        double grid_eps;
+        std::pair<double, double> grid_range;
+        std::string device;
+        torch::nn::ModuleList convs;
+};
+
+class KAN_Convolution : public torch::nn::Module {
+    public:
+        KAN_Convolution(
+            std::pair<int, int> kernel_size = {2,2},
+            std::pair<int, int> stride = {1,1},
+            std::pair<int, int> padding = {0,0},
+            std::pair<int, int> dilation = {1,1},
+            int grid_size = 5,
+            int spline_order = 3,
+            double scale_noise = 0.1,
+            double scale_base = 1.0,
+            double scale_spline = 1.0,
+            torch::nn::SiLU base_activation = torch::nn::SiLU(),
+            double grid_eps = 0.02,
+            std::pair<double, double> grid_range = {-1, 1},
+            std::string device = "cpu");
+
+        torch::Tensor forward(const torch::Tensor& x);
+        double regularization_loss(double regularize_activation = 1.0, double regularize_entropy = 1.0);
+    
+    private:
+        std::pair<int, int> kernel_size;
+        std::pair<int, int> stride;
+        std::pair<int, int> padding;
+        std::pair<int, int> dilation;
+        int grid_size;
+        int spline_order;
+        double scale_noise;
+        double scale_base;
+        double scale_spline;
+        torch::nn::SiLU base_activation;
+        double grid_eps;
+        std::pair<double, double> grid_range;
+        std::string device;
+        KANLinear conv;
+};
+
+
+#endif // KAN_CONV_H
--- include/layers/convolution.h
+++ include/layers/convolution.h
@@ -4,7 +4,9 @@
 #include <torch/torch.h>
 #include <vector>
 #include <tuple>
+#include "../../include/layers/KAN_linear.h"
 
+namespace convolution {
 // Function declarations
 std::tuple<int, int, int, int> calc_out_dims(
     const torch::Tensor& matrix,
@@ -15,5 +17,21 @@ std::tuple<int, int, int, int> calc_out_dims(
 );
 
 torch::Tensor add_padding(const torch::Tensor& matrix, const std::pair<int, int>& padding);
+
+torch::Tensor multiple_convs_kan_conv2d();
+
+torch::Tensor kan_conv2d(
+        const torch::Tensor& x,
+        const KANLinear& conv,  // Change this to KANLinear, which matches the implementation
+        const std::pair<int, int>& kernel_size = {2, 2},
+        const std::pair<int, int>& stride = {1, 1},
+        const std::pair<int, int>& dilation = {1, 1},
+        const std::pair<int, int>& padding = {0, 0},
+        const std::string& device = "cpu"
+    );
+
+};
+
+
 
 #endif // CONVOLUTION_H
